225da401504e52144b654119f4b99ce889641d38
danauerbach <dauerbach@ucsd.edu>
2025-02-04 16:21:22 -0800

    reqmon interrupt flags; encode scheme

--- sendreq.py
+++ sendreq.py
@@ -17,22 +17,8 @@ import json
 
 import paho.mqtt.client as mqtt
 
-# REQMON_CFG = {
-#     "mqtt": {
-#         "HOST": 'iot.tessa-obs.net',
-#         "PORT": 8883,
-#         "root-CA": 'AmazonRootCA1.pem',
-#         "key": 'tessa-reqmon.private.pem.key',
-#         "cert": 'tessa-reqmon.pem.crt',
-#         "req_topic": 'tessa/req',
-#         "reqack_topic": 'teesa/reqack',
-#         "reqmon_client_id": "tessa-reqmon",
-#         "reqack_client_id": "tessa-reqack",
-#     }
-# }
 
-
-def paho_client_setup(endpoint, port, client_id, root_ca, cert, key, req_topic, req_q):
+def paho_client_setup(endpoint, port, client_id, root_ca, cert, key, ack_topic, req_q):
 
     def is_valid_request(msg):
 
@@ -45,16 +31,15 @@ def paho_client_setup(endpoint, port, client_id, root_ca, cert, key, req_topic, 
     def on_message(client, userdata, message):
 
         msg_str = message.payload.decode('utf-8')
-        print(f'reqmon:on_message: {msg_str}')
+        print(f'{client_id}:on_message: {msg_str}')
         msg_dict = json.loads(msg_str)
-        if is_valid_request(msg_dict):
-            req_q.put(msg_dict)
+        req_q.put(msg_dict)
 
     def on_connect(client, userdata, flags, rc):
         if rc==0:
-            print("reqmon:on_connect: connected OK: {client}")
+            print("{client_id}:on_connect: connected OK: {client}")
         else:
-            print("reqmon:on_connect: Bad connection for {client} Returned code: ", rc)
+            print("{client_id}:on_connect: Bad connection for {client} Returned code: ", rc)
             client.loop_stop()
 
     def on_disconnect(client, userdata, rc):
@@ -64,35 +49,34 @@ def paho_client_setup(endpoint, port, client_id, root_ca, cert, key, req_topic, 
         print("Subscribed: "+str(mid)+" "+str(granted_qos))
 
     def on_publish(client, userdata, mid):
-        print("reqmon:on_connect: {client} mid= "  ,mid)
+        print("{client_id}:on_connect: {client} mid= "  ,mid)
 
 
-    reqmon_client = mqtt.Client(client_id=client_id)
-    reqmon_client.tls_set(root_ca, certfile=cert, keyfile=key, tls_version=ssl.PROTOCOL_TLSv1_2, cert_reqs=ssl.CERT_REQUIRED)
-    reqmon_client.on_connect = on_connect
-    reqmon_client.on_disconnect = on_disconnect
-    reqmon_client.on_subscribe = on_subscribe
-    reqmon_client.on_publish = on_publish
-    reqmon_client.on_message = on_message
-    reqmon_client.connect(endpoint, port)
+    req_client = mqtt.Client(client_id=client_id)
+    req_client.tls_set(root_ca, certfile=cert, keyfile=key, tls_version=ssl.PROTOCOL_TLSv1_2, cert_reqs=ssl.CERT_REQUIRED)
+    req_client.on_connect = on_connect
+    req_client.on_disconnect = on_disconnect
+    req_client.on_subscribe = on_subscribe
+    req_client.on_publish = on_publish
+    req_client.on_message = on_message
+    req_client.connect(endpoint, port)
 
-    res, _ = reqmon_client.subscribe(req_topic, qos=1)
+    res, _ = req_client.subscribe(ack_topic, qos=1)
     if res != mqtt.MQTT_ERR_SUCCESS:
-        print(f'{client_id}: ERROR subscribing to {req_topic}')
+        print(f'{client_id}: ERROR subscribing to {ack_topic}')
         print(f'{client_id}: shutting down')
         # quit_evt.set()
         time.sleep(.25)
         return None
     else:
-        reqmon_client.loop_start()
+        req_client.loop_start()
 
-    return reqmon_client
+    return req_client
 
 
 def interrupt_handler(signum, frame):
 
     quit_evt.set()
-
     time.sleep(1)
     # sys.exit(0)
 
@@ -100,6 +84,36 @@ def interrupt_handler(signum, frame):
 if __name__ == '__main__':
 
     signal.signal(signal.SIGINT, interrupt_handler)
+
+    parser = argparse.ArgumentParser(description='construct and send data request to topic "tessa/request" and listenf or ACK on topic "tessa/reqack"')
+    parser.add_argument("sta", action='store', required=True, help='station code to request data from')
+    parser.add_argument("beg", action='store', required=True, help='Start time (iso8660) of requested data segment')
+    parser.add_argument("end", action='store', required=True, help='End time (iso8660) of requested data segment')
+
+    args = parser.parse_args()
+
+    sta = args.sta.upper()
+    beg = args.beg.upper()
+    if not beg.endswith('Z'):
+        beg += 'Z'
+    begep = datetime.datetime.fromisoformat(beg).timestamp()
+    end = args.end.upper()
+    if not end.endswith('Z'):
+        end += 'Z'
+    endep = datetime.datetime.fromisoformat(end).timestamp()
+
+    rid = datetime.datetime.now(tz=datetime.timezone.utc).timestamp()
+
+    msg = {
+        'rid': rid,
+        'sta': sta,
+        'beg': begep,
+        'end': endep
+    }
+
+    msg_str = json.dumps(msg)
+    print(f'msg_str: {msg_str}')
+
 
     aws_dir = os.getenv('TESSA_AWS_DIR')
     if not aws_dir:
@@ -111,18 +125,13 @@ if __name__ == '__main__':
         print('ERROR: TESSA_WG_THING_NAME env var does not exist. Quitting....', file=sys.stderr)
         sys.exit(1)
 
-    DATA_ROOT_DIR = os.getenv('TESSA_DATA_ROOT')
-    if not DATA_ROOT_DIR:
-        print('ERROR: TESSA_DATA_ROOT env var does not exist. Quitting....', file=sys.stderr)
-        sys.exit(1)
-
     ENDPOINT = 'iot.tessa-obs.net'
     PORT = 8883
     CERT = os.path.join(aws_dir, f'{thing_name}.pem.crt')
     KEY = os.path.join(aws_dir, f'{thing_name}.private.pem.key')
     ROOT_CA = os.path.join(aws_dir, 'AmazonRootCA1.pem')
 
-    CLIENT_ID = 'tessa-wg-reqmon'
+    CLIENT_ID = thing_name
     REQ_TOPIC = 'tessa/request'
     ACK_TOPIC = 'tessa/reqack'
 
@@ -133,8 +142,9 @@ if __name__ == '__main__':
     # to main thread that writes to disk and sends ack
     req_q = queue.Queue()
 
-    reqmon_client = paho_client_setup(ENDPOINT, PORT, CLIENT_ID, ROOT_CA, CERT, KEY, REQ_TOPIC, quit_evt)
+    req_client = paho_client_setup(ENDPOINT, PORT, CLIENT_ID, ROOT_CA, CERT, KEY, ACK_TOPIC, req_q)
 
+    req_client.publish(REQ_TOPIC, payload=msg_str.encode('utf-8'), qos=1)
 
     while not quit_evt.is_set():
 
@@ -143,35 +153,22 @@ if __name__ == '__main__':
             req_q.task_done()
 
         except queue.Empty as e:
-            # print(f'reqmon:main: Ignoring empty request.')
+            print(f'No ACK received yet....')
             continue
 
         except Exception as e:
             print(f'reqmon:main: ERROR receiving data msg: {e}')
             continue
 
-        # construct ACK response
-        cmd = {
-            'rid': req_dict['rid'],
-            'rcvd_ts': datetime.datetime.now(tz=datetime.timezone.utc).timestamp(),
-            'status': 'OK'
-        }
-        reqmon_client.publish(ACK_TOPIC, json.dumps(cmd).encode("utf-8"), qos=1)
+        #### CHECK FOR status='OK' ACK msg ########
+        # TODO TODO
+        
 
+        
         time.sleep(1)
 
-    quit_evt.wait()
-
     # gives threads a chance to exit cleanly    
-    reqmon_client.loop_stop()
+    req_client.loop_stop()
 
-    # start threads for each client with reference to req_q.
-    # reqmon_client thread will:
-    #   listen for requets from AWS IoT and
-    #   vaidate request
-    # if valid, send to req ack client thread using req_q Queue
 
-    # reqack_client thread will
-    #   Listen got requests over internal queue
-    #   Serialize request to disk  
-    #   Send ACk message to 'tessa/reqack' topic at AWS IoT with RequestID of incoming request.
+
